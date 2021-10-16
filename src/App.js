aca3d0285432802c110c5d2da02b72297c4107f2
ammarr <ahmedmahmoud9597@gmail.com>
2021-10-16 18:05:31 +0200

    link server

--- src/App.js
+++ src/App.js
@@ -31,8 +31,8 @@ function App () {
   // const othersVideos = useRef([])
 
   useEffect(() => {
-   const newSocket = io(`http://localhost:4000/`)
-   //const newSocket = io(`https://stream-squ-server.herokuapp.com/`)
+  // const newSocket = io(`http://localhost:4000/`)
+   const newSocket = io(`https://stream-squ-server.herokuapp.com/`)
     setSocket(newSocket)
     return () => newSocket.close()
   }, [setSocket])
